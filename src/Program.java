5773abeef3efef433cf5979e316d4a110d22a07d
Ilya Bikmeev <ilyabikmeev@gmail.com>
2022-12-12 22:28:07 +0500

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        FrequencyCounter counter = new FrequencyCounter("War and piece.txt");
+        counter.printStat();    //печать статистики на экран
+
+
+    }
+}
